3e431123f1317b960d6b06c35bded2c09ccd27e9
Henrik Ohman <speeph@gmail.com>
2013-09-03 22:05:44 +0200

    Changed to use the latest available CentOS 6 image.

--- defaults.sh
+++ defaults.sh
@@ -2,7 +2,7 @@ default_nodes=4
 default_project=atlasgce
 default_zone=europe-west1-b
 default_machine=n1-standard-1-d
-default_image=projects/centos-cloud/global/images/centos-6-v20130813
+default_image=centos-6
 
 nodes=$default_nodes
 project=$default_project
