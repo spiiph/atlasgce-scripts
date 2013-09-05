7e670ac4dc481173b1f63ff129b4aa1604fd46b8
Henrik Ohman <speeph@gmail.com>
2013-09-05 14:48:47 +0200

    Added Cloud Scheduler worker node configuration manifest.

---
+++ cloudscheduler/gce_node_csworker.pp
@@ -0,0 +1,32 @@
+# Mounts and node template for a gce worker node
+
+# Cache for CVMFS
+mount {'/var/cache/cvmfs2':
+    device => '/dev/vg00/lv_cvmfs',
+    fstype => 'ext4',
+    options => 'defaults',
+    ensure => mounted,
+    dump => 1,
+    pass => 2,
+}
+
+# Cache for scheduler
+mount {'/var/lib/condor':
+    device => '/dev/vg00/lv_condor',
+    fstype => 'ext4',
+    options => 'defaults',
+    ensure => mounted,
+    dump => 1,
+    pass => 2,
+}
+
+class { 'gce_node':
+  head => 'to.be.contextualized.by.cloud.scheduler',
+  role => 'csnode',
+  condor_pool_password => undef,
+  condor_use_gsi => true,
+  condor_slots => 1,
+  use_xrootd => false,
+  atlas_site => 'CERN-PROD',
+  require => Mount['/var/cache/cvmfs2', '/var/lib/condor'],
+}
